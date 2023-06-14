7b68bc3b615f59468f644b8b951c3b972d8b9d45
Beckham Wilson <notbeckhamster@gmail.com>
2023-06-15 11:54:03 +1200

    Testing CI

--- src/PiecePanel.java
+++ src/PiecePanel.java
@@ -4,7 +4,6 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.GridBagLayout;
-
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
